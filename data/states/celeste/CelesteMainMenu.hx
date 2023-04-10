1927c91165276e2ff01d12d6ceab28dfdc366814
NeeEoo <NeeEoo@users.noreply.github.com>
2023-04-10 17:02:19 +0200

    Working on the buttons a bit

--- data/states/celeste/CelesteMainMenu.hx
+++ data/states/celeste/CelesteMainMenu.hx
@@ -1,56 +1,58 @@
+import flixel.FlxG;
 import flixel.text.FlxText;
 import flixel.FlxSprite;
 import funkin.system.Paths;
 import flixel.text.FlxTextBorderStyle;
+import MenuButton;
 
 var ref_img:FlxSprite;
 
-var text_climb:FlxText;
-var text_pico:FlxText;
-var text_options:FlxText;
-var text_credits:FlxText;
-var text_exit:FlxText;
+var text_climb:MenuButton;
+var text_pico:MenuButton;
+var text_options:MenuButton;
+var text_credits:MenuButton;
+var text_exit:MenuButton;
+var textElements:Array<MenuButton> = [];
 
 function create() {
-    var tempbg = new FlxSprite().makeGraphic(1, 1, 0xFF666666);
-    tempbg.scale.set(FlxG.width, FlxG.height);
-    tempbg.updateHitbox();
-    add(tempbg);
+	var tempbg = new FlxSprite().makeGraphic(1, 1, 0xFF666666);
+	tempbg.scale.set(FlxG.width, FlxG.height);
+	tempbg.updateHitbox();
+	add(tempbg);
 
-    ref_img = new FlxSprite().loadGraphic(Paths.image('- ref'));
-    ref_img.setGraphicSize(FlxG.width, FlxG.height);
-    ref_img.updateHitbox();
-    ref_img.antialiasing = true;
-    ref_img.screenCenter();
-    add(ref_img);
+	ref_img = new FlxSprite().loadGraphic(Paths.image('- ref'));
+	ref_img.setGraphicSize(FlxG.width, FlxG.height);
+	ref_img.updateHitbox();
+	ref_img.antialiasing = true;
+	ref_img.screenCenter();
+	add(ref_img);
 
-    var baseX = 164;
-    var offset = 53.5;
+	var baseX = 164;
+	var offset = 53.5;
 
-    text_climb = new FlxText(125, 260, 0, "CLIMB");
-    text_pico = new FlxText(baseX, 332, 0, "PICO-8");
-    text_options = new FlxText(baseX, text_pico.y + offset, 0, "Options");
-    text_credits = new FlxText(baseX, text_options.y + offset, 0, "Credits");
-    text_exit = new FlxText(baseX, text_credits.y + offset, 0, "Exit");
+	text_climb = new MenuButton(125, 260.7, 0, "CLIMB", 45);
+	text_pico = new MenuButton(baseX, 332.7, 0, "PICO-8", 30);
+	text_options = new MenuButton(baseX, text_pico.y + offset, 0, "Options", 30);
+	text_credits = new MenuButton(baseX, text_options.y + offset, 0, "Credits", 30);
+	text_exit = new MenuButton(baseX, text_credits.y + offset, 0, "Exit", 30);
 
-    for(text in [text_climb, text_pico, text_options, text_credits, text_exit]) {
-        text.font = Paths.font('Renogare-Regular.otf');
-        text.size = (text == text_climb) ? 45 : 30;
-        text.size *= 2;
-        text.scale.set(0.5, 0.5);
-        text.updateHitbox();
-        text.fieldWidth = (text.width + 10) * 2;
-        text.alignment = "center";
-        text.antialiasing = true;
+	FlxG.game.debugger.console.registerObject("text_climb", text_climb);
+	FlxG.game.debugger.console.registerObject("text_pico", text_pico);
+	FlxG.game.debugger.console.registerObject("text_options", text_options);
+	FlxG.game.debugger.console.registerObject("text_credits", text_credits);
+	FlxG.game.debugger.console.registerObject("text_exit", text_exit);
 
-        text.borderStyle = FlxTextBorderStyle.OUTLINE;
-        text.borderSize = 2;
-        text.borderColor = 0xFF000000;
+	textElements = [text_climb, text_pico, text_options, text_credits, text_exit];
 
-        add(text);
-        trace(text.text + " loaded");
-    }
+	for(text in textElements) {
+		add(text);
+		trace(text.text + " loaded");
+	}
+
+	changeSelection(0);
 }
+
+var curSelected = 0;
 
 var ri = 0;
 var r = [1, 0.5, 0];
@@ -59,13 +61,27 @@ var r = [1, 0.5, 0];
 // oh shit!! god i love funkscop
 
 function update() {
-    ref_img.alpha = r[ri];
-    if (FlxG.keys.justPressed.J)
-        ri = (ri + 1) % r.length;
+	ref_img.alpha = r[ri];
+	if (FlxG.keys.justPressed.J)
+		ri = (ri + 1) % r.length;
 
-    if (FlxG.keys.justPressed.F4) // changing to f4 bc it don't work :(
-        FlxG.switchState(new ModState("celeste/CelesteMainMenu"));
+	if (FlxG.keys.justPressed.F4) // changing to f4 bc it don't work :(
+		FlxG.switchState(new ModState("celeste/CelesteMainMenu"));
 
-    if (FlxG.keys.justPressed.ESCAPE)
-        FlxG.switchState(new MainMenuState());
+	if (FlxG.keys.justPressed.ESCAPE)
+		FlxG.switchState(new MainMenuState());
+}
+
+function mod(n:Int, m:Int) {
+	return ((n % m) + m) % m;
+}
+
+function changeSelection(change) {
+	curSelected = mod(curSelected + change, textElements.length);
+
+	var i = 0;
+	for(text in textElements) {
+		text.targetItem = i - curSelected;
+		i++;
+	}
 }
--- source/MenuButton.hx
+++ source/MenuButton.hx
@@ -1,9 +1,49 @@
-class MenuButton extends flixel.FlxSprite {
-    public function new(X:Float = 0, Y:Float = 0, FieldWidth:Float = 0, ?Text:String, Size:Int = 16) {
-        super(X, Y, FieldWidth, Text, Size);
+import flixel.FlxG;
+import flixel.text.FlxText;
+import flixel.FlxSprite;
+import funkin.system.Paths;
+import flixel.text.FlxTextBorderStyle;
+
+class MenuButton extends flixel.text.FlxText {
+    public function new(X:Float = 0, Y:Float = 0, FieldWidth:Float = 0, ?Text:String, Size:Int = 8) {
+    	super(X, Y, FieldWidth, Text, Size);
+
+		font = Paths.font('Renogare-Regular.otf');
+		size *= 2;
+		scale.set(0.5, 0.5);
+		updateHitbox();
+		fieldWidth = (width + 10) * 2;
+		alignment = "center";
+		antialiasing = true;
+
+		borderStyle = FlxTextBorderStyle.OUTLINE;
+		borderSize = 2;
+		borderColor = 0xFF000000;
+
+		active = true;
+
+		trace("loaded");
     }
 
-    override function update() {
+	var targetItem = 0;
 
+	var colorTimer = 0.3;
+	var isGreen = true;
+
+    public override function update(elapsed:Float) {
+		super.update(elapsed);
+
+		trace(targetItem);
+
+		if(targetItem == 0) {
+			colorTimer -= elapsed;
+			while(colorTimer <= 0) {
+				colorTimer += 0.3;
+				isGreen = !isGreen;
+			}
+			color = isGreen ? 0xFF00FF00 : 0xFFffff00;
+		} else {
+			color = 0xFFFFFFFF;
+		}
     }
 }
